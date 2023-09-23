b59e44502d962c43eef9d65f0eb82d3022114d4a
Ishaan1817 <132992855+Ishaan1817@users.noreply.github.com>
2023-09-23 16:55:45 +0530

    Add files via upload

---
+++ src/utils/firebase.js
@@ -0,0 +1,71 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider,signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import {getFirestore, doc,  getDoc, setDoc} from 'firebase/firestore'
+    
+const firebaseConfig = {
+  apiKey: "AIzaSyAwURGyTcun94QfifmNfVKuAax-e76VBe0",
+  authDomain: "login-df11d.firebaseapp.com",
+  projectId: "login-df11d",
+  storageBucket: "login-df11d.appspot.com",
+  messagingSenderId: "1013633476070",
+  appId: "1:1013633476070:web:c16ece7f1960e6a4e02227",
+  measurementId: "G-61B5NCZRH7"
+};
+
+  const firebaseapp = initializeApp(firebaseConfig);
+  const provider= new GoogleAuthProvider();
+ 
+  provider.setCustomParameters({
+  prompt:"select_account"
+});
+  
+  export const auth=getAuth();
+  export const signInWithGooglePopup =()=> signInWithPopup(auth,provider)
+  export const db =getFirestore();
+  export const createuserdocfromAuth = async(userAuth, additionalInformation ={}) =>
+
+{
+  if(!userAuth.email) return;
+
+  const userDocRef=doc (db, 'users', userAuth.uid);
+  console.log(userDocRef)
+
+
+const userSnapShots = await getDoc(userDocRef);
+console.log(userSnapShots)
+console.log(userSnapShots.exists())
+
+if(!userSnapShots.exists())
+{
+   const {displayName, email} =userAuth
+   const createdAt = new Date();
+   try{
+    await setDoc(userDocRef,{
+   displayName,
+   email,
+   createdAt,
+   ...additionalInformation
+    })
+  }
+    catch(error){
+    console.log('error in creating', error.message)
+    }
+
+   }
+   return userDocRef;
+}
+
+export async function createAuthUserWithEmailAndPassword (email,password)
+{ 
+  if(!email || !password) 
+  return
+  return await createUserWithEmailAndPassword(auth,email,password)
+}
+
+export async function signinAuthUserWithEmailAndPassword (email,password)
+{ 
+  if(!email || !password) 
+  return
+  return await signInWithEmailAndPassword(auth,email,password)
+}
+
